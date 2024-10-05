6d2eb96556d6f90eb883bf1cf8499383571e2a09
DungGramer <dung.dev.gramer@gmail.com>
2024-10-05 16:53:55 +0700

    Authentication & Database

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,10 +2,16 @@
 
 import { Logo } from "@/app/(marketing)/_components/logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 
 const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +23,29 @@ const Navbar = () => {
     >
       <Logo />
       <div className='md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2'>
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode='modal'>
+              <Button variant='ghost' size='sm'>
+                Login
+              </Button>
+            </SignInButton>
+            <SignInButton mode='modal'>
+              <Button variant='ghost' size='sm'>
+                Get Jotion free
+              </Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant='ghost' size='sm' asChild>
+              <Link href='/documents'>Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl='/' />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
