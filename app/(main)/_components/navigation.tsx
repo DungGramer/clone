6fd411ef44c02bed9e8effb9c827958438990f50
DungGramer <dung.dev.gramer@gmail.com>
2024-10-20 16:00:20 +0700

    Navbar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -2,6 +2,7 @@
 
 import DocumentList from "@/app/(main)/_components/document-list";
 import Item from "@/app/(main)/_components/item";
+import Navbar from "./navbar";
 import TrashBox from "@/app/(main)/_components/trash-box";
 import UserItem from "@/app/(main)/_components/user-item";
 import {
@@ -23,7 +24,7 @@ import {
   Settings,
   Trash,
 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { useParams, usePathname } from "next/navigation";
 import { ElementRef, MouseEvent, useEffect, useRef, useState } from "react";
 import { toast } from "sonner";
 import { useMediaQuery } from "usehooks-ts";
@@ -31,6 +32,7 @@ import { useMediaQuery } from "usehooks-ts";
 const Navigation = () => {
   const settings = useSettings();
   const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const create = useMutation(api.documents.create);
@@ -180,14 +182,18 @@ const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className='bg-transparent px-3 py-2 w-full'>
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              className='h-6 w-6  text-muted-foreground'
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className='bg-transparent px-3 py-2 w-full'>
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                className='h-6 w-6  text-muted-foreground'
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
