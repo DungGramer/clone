16c1d538d98077270261a2202d49e7ced139bee2
DungGramer <dung.dev.gramer@gmail.com>
2024-12-02 10:19:44 +0700

    add page

--- src/app/editor/[projectId]/page.tsx
+++ src/app/editor/[projectId]/page.tsx
@@ -1,32 +1,8 @@
 "use client";
-import { useEditor } from "@/features/editor/hooks/use-editor";
-import { fabric } from "fabric";
-import { useEffect, useRef } from "react";
+import { Editor } from "@/features/editor/components/editor";
 
 const EditorProjectIdPage = () => {
-  const { init } = useEditor();
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const canvas = new fabric.Canvas(canvasRef.current!, {
-      controlsAboveOverlay: true, //? Can control outside of the workspace
-      preserveObjectStacking: true,
-    });
-
-    init({
-      initialCanvas: canvas,
-      initialContainer: containerRef.current!,
-    });
-  }, [init]);
-
-  return (
-    <div className='h-full flex flex-col'>
-      <div className='flex-1 h-full bg-muted' ref={containerRef}>
-        <canvas ref={canvasRef} />
-      </div>
-    </div>
-  );
+  return <Editor />;
 };
 
 export default EditorProjectIdPage;
